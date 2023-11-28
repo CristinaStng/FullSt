6a042c3b4603ea077d9100c4fd48b6d5edeaf8d3
cristinher035 <cristinher035@gmail.com>
2023-11-27 21:49:11 -0500

    subiendo FulsTarck

---
+++ server/constants.js
@@ -0,0 +1,16 @@
+const DB_USER = "cristina"
+const DB_PASSWORD = "9wneZXX2DuhA0lZM"
+const DB_HOST = "cluster0.zj9dvv6.mongodb.net"
+
+const API_VERSION = "v1"
+const IP_SERVER = "localhost"
+const JWT_SECRET_KEY = "adgsdfgwe1321das54adf121346asd465981"
+
+module.exports ={
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER,
+    JWT_SECRET_KEY
+}
